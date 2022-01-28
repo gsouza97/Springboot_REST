f8e32e87d9eb7993e30a616ce86c123c4485977b
Gabriel Souza <gabrielss_@outlook.com>
2022-01-28 11:59:13 -0300

    pegando os dados direto do DB

--- src/main/java/com/example/demo/services/StudentService.java
+++ src/main/java/com/example/demo/services/StudentService.java
@@ -4,13 +4,22 @@ import java.time.LocalDate;
 import java.util.List;
 
 import com.example.demo.model.Student;
+import com.example.demo.repository.StudentRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(new Student(1L, "Gabriel", "gabriel@gmail.com", LocalDate.of(1997, 01, 03), 25));
+        return studentRepository.findAll();
     }
 }
