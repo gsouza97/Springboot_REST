09b8a61bd6b94255fa5fb5b37b732544bca4d736
Gabriel Souza <gabrielss_@outlook.com>
2022-01-29 23:55:48 -0300

    atualizando Student no DB

--- src/main/java/com/example/demo/services/StudentService.java
+++ src/main/java/com/example/demo/services/StudentService.java
@@ -1,7 +1,10 @@
 package com.example.demo.services;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+
+import javax.transaction.Transactional;
 
 import com.example.demo.model.Student;
 import com.example.demo.repository.StudentRepository;
@@ -40,6 +43,34 @@ public class StudentService {
             studentRepository.deleteById(id);
         } else {
             throw new IllegalStateException("student with id" + id + "does not exists");
+        }
+    }
+
+    @Transactional
+    public void updateStudent(Long id, String name, String email) {
+
+        // Verifica se tem o estudante com o id passado
+        Student student = studentRepository.findById(id)
+                .orElseThrow(() -> new IllegalStateException("student with id" + id + "does not exist"));
+
+        // Se o nome não for nulo e maior do que 0 e se o nome passado não for igual ao
+        // nome já armazenado, atualiza o nome
+        if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)) {
+            student.setName(name);
+        }
+
+        // Se o email não for nulo e maior do que 0 e se o email passado não for igual
+        // ao
+        // email já armazenado, atualiza o email
+        if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+
+            // Verifica se aquele email já foi utilizado
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+
+            if (studentOptional.isPresent()) {
+                throw new IllegalStateException("email taken");
+            }
+            student.setEmail(email);
         }
     }
 }
